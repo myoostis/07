808f1ef5fb059c381d937595244f39bad8bd0d32
myoostis <parkgin2011@naver.com>
2022-10-18 16:28:41 +0900

    02

--- main.c
+++ main.c
@@ -5,16 +5,13 @@
 
 int main(void) {
 	
-	int flag = 1;
-	int y;
+	int i;
 	
-	while(flag!=0){
-		
-		y=3;
-		flag = 0;
+	for(i=0;i<5;i++){
+		int temp = 1;
+		printf("temp = %d\n", temp);
+		temp++;
 	}
-	
-	y=4;
 	return 0;
 }
 
